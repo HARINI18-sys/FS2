8a6909a4779bdde05e58753c8152ef0893d96c79
HARINI18-sys <harinisaravanan1808@gmail.com>
2025-06-19 12:07:42 +0530

    complete loops topic

--- operator.js
+++ operator.js
@@ -9,7 +9,7 @@ console.log(10<=2);
 console.log("2" == 2);
 console.log("2"===2);
 console.log("2"!==2);
-console.log(2>3 && 3>2)
-console.log(2<3 && 3>2)
-console.log(2>3 || 3>2)
-console.log(2<3 || 3>2)
+console.log(2>3 && 3>2);
+console.log(2<3 && 3>2);
+console.log(2>3 || 3>2);
+console.log(2<3 || 3>2);
