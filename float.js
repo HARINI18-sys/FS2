1702fdfae436f43809ceef8b9ad5985891a5dad2
HARINI18-sys <harinisaravanan1808@gmail.com>
2025-06-19 10:55:07 +0530

    complete operators topic

---
+++ float.js
@@ -0,0 +1,5 @@
+let n1=2.1;
+let n2=5.1;
+console.log(n1+n2);
+let r=((21+51)/10).toFixed(2);
+console.log(r);
---
+++ operator.js
@@ -0,0 +1,15 @@
+console.log(10+2);
+console.log(10-2);
+console.log(10/2);
+console.log(10*2);
+console.log(10<2);
+console.log(10>2);
+console.log(10>=2);
+console.log(10<=2);
+console.log("2" == 2);
+console.log("2"===2);
+console.log("2"!==2);
+console.log(2>3 && 3>2)
+console.log(2<3 && 3>2)
+console.log(2>3 || 3>2)
+console.log(2<3 || 3>2)
